d4207905815cbf97adba8cfc781b7494dbb40b9c
sunren <sr02310075@taobao.com>
2026-03-24 19:58:01 +0800

    修改
    Signed-off-by: sunren <sr02310075@taobao.com>

--- Package.swift
+++ Package.swift
@@ -15,18 +15,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TanxSDK",
-            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/releases/download/3.7.27/TanxSDK.xcframework.zip",
+            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/releases/download/3.7.28/TanxSDK.xcframework.zip",
             checksum: "0f18002f09c409a5d977cae8e40298374be6ea41fff4044f17bc73f81fc06df4"
         ),
         .binaryTarget(
             name: "TNXASDK",
-            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/releases/download/3.7.27/TNXASDK.xcframework.zip",
-            checksum: "0bfe2bb2a8843394c0a3eca7652fe580e0c404a07f2b18f4d71e9b0999d04938"
+            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/releases/download/3.7.28/TNXASDK.xcframework.zip",
+            checksum: "4ef853880d6db57fa6f44d218b7e2a35b3ca89002b6eecc805e406ae166bb6fd"
         ),
         .binaryTarget(
             name: "TanxMonitor",
-            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/releases/download/3.7.27/TanxMonitor.xcframework.zip",
-            checksum: "4ef853880d6db57fa6f44d218b7e2a35b3ca89002b6eecc805e406ae166bb6fd"
+            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/releases/download/3.7.28/TanxMonitor.xcframework.zip",
+            checksum: "0bfe2bb2a8843394c0a3eca7652fe580e0c404a07f2b18f4d71e9b0999d04938"
         )
     ]
 )
